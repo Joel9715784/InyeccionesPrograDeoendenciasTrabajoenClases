28e3bbe21046fa50c80a313d829f0b5e35370625
Joel9715784 <77417721+Joel9715784@users.noreply.github.com>
2021-01-15 13:47:43 -0500

    Tarea en clase
    Tarea en clase

---
+++ Inyeccion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Interfaces;
+using Negocio;
+using Entidades;
+namespace InyecciondeDependenciasApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IPersistencia persistencia = null; 
+           
+
+
+            Secretaria secretaria = new Secretaria(persistencia);
+            Estudiante estudiante = new Estudiante("Joel", "Kathe");
+            Curso curso = new Curso("Programacion Visual");
+
+            secretaria.Matricular(estudiante, curso);
+
+
+        }
+    }
+}
